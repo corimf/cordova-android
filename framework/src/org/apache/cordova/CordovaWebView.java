5d9db51fc4b19f393aeba48cd321e870e30a1402
Karen Tran <ktop500@gmail.com>
2016-01-13 22:34:49 -0500

    Don't Call pauseTimers() in startActivityForResult()

--- framework/src/org/apache/cordova/CordovaWebView.java
+++ framework/src/org/apache/cordova/CordovaWebView.java
@@ -743,7 +743,7 @@ public class CordovaWebView extends WebView {
    
     }
     
-    public void handleResume(boolean keepRunning, boolean activityResultKeepRunning)
+    public void handleResume(boolean keepRunning)
     {
 
         this.loadUrl("javascript:try{cordova.fireDocumentEvent('resume');}catch(e){console.log('exception firing resume event from native');};");
